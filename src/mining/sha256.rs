67c07e34ebbaf98d22a6be41cbd5f80679f2a4a1
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-28 14:46:22 -0300

    Update sha256.rs

--- src/mining/sha256.rs
+++ src/mining/sha256.rs
@@ -1,1 +1,29 @@
+extern crate bdk;
+extern crate bitcoin_hashes;
+
+use bdk::blockchain::noop_progress;
+use bdk::bitcoin::network::constants::Network;
+use bdk::bitcoin::util::address::Address;
+use bdk::bitcoin::util::key::PrivateKey;
+use bdk::Wallet;
+use bitcoin_hashes::sha256;
+use std::str::FromStr;
+
+fn main() {
+    // Set up the BDK wallet
+    let descriptor = "wpkh(tprv8ZgxMBicQKsPdY5HfY4Upd3h3tqDnxDaJkV5mbD66FhUzFJjGSV9aG7rJ3mDnAtwqX8J4T5Hf5X1sgeTrmFa6cav8JCYeNX1LG9HJWx7gZU/0/*)";
+    let network = Network::Testnet;
+    let wallet = Wallet::new_offline(descriptor, None, network, None).unwrap();
+
+    // Generate a new private key
+    let private_key = wallet.keychain(network).derive_private_key(0).unwrap();
+    println!("Private Key: {}", private_key);
+
+    // Convert the private key to a byte array
+    let private_key_bytes = private_key.private_key.to_bytes();
+
+    // Calculate SHA256 hash
+    let hash = sha256::Hash::hash(&private_key_bytes);
+    println!("SHA256 Hash: {:?}", hash);
+}
 
