7aaa562834b42f4b8534f5728dd255a4ff78bb2e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-28 14:50:42 -0300

    Update pool.rs

--- src/mining/pool.rs
+++ src/mining/pool.rs
@@ -1,1 +1,51 @@
+extern crate bitcoin;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+
+use bitcoin::blockdata::block::BlockHeader;
+use bitcoin::network::serialize::RawDecoder;
+use bitcoin::network::serialize::BitcoinHash;
+use bitcoin_hashes::sha256d;
+use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+// Define pool-related structs (e.g., WorkResponse, SubmitResponse)
+
+fn main() {
+    // Connect to the mining pool's API to get work
+    let pool_url = "https://mining-pool.example/api/work";
+    let client = Client::new();
+    let response = client.get(pool_url).send().expect("Failed to fetch work");
+
+    // Deserialize the JSON response
+    let work_response: WorkResponse = serde_json::from_str(&response.text().expect("Failed to read response")).expect("Failed to deserialize JSON");
+
+    // Perform proof of work calculations
+    let target = bitcoin::network::constants::MAX_TARGET;
+    let mut nonce = 0;
+    loop {
+        let hash = sha256d::Hash::hash(&work_response.block_header.serialize_with_nonce(nonce));
+        if hash <= target {
+            // Submit the solution to the pool
+            let solution = Solution {
+                block_header: work_response.block_header,
+                nonce: nonce,
+            };
+            let submit_url = "https://mining-pool.example/api/submit";
+            let submit_response = client.post(submit_url).json(&solution).send().expect("Failed to submit solution");
+            let submit_response: SubmitResponse = serde_json::from_str(&submit_response.text().expect("Failed to read submit response")).expect("Failed to deserialize JSON");
+            
+            if submit_response.accepted {
+                println!("Solution accepted by the pool!");
+            } else {
+                println!("Solution rejected by the pool.");
+            }
+            
+            break;
+        }
+        nonce += 1;
+    }
+}
 
