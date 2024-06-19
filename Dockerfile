b076eff6983682c81378ec261c09eed6270d7689
223880 <rafael.segala45@gmail.com>
2024-06-19 07:15:58 -0300

    fix docker and release bolt11

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
-$ Lightning
-RUN VERSION = 0.0.123
+FROM rust-Lightning
+FROM rust-bitcoin 
+FROM bdk 
+RUN cargo install --git https://github.com/AreaLayer/HashPool.git --branch master --locked
+RUN cargo install --git https://github.com/AreaLayer/HashPool.git --branch master --locked --features=bitcoin
+RUN cargo install --git https://github.com/AreaLayer/HashPool.git --branch master --locked --features=lightning
+RUN cargo install --git https://github.com/AreaLayer/HashPool.git --branch master --locked --features=bitcoin,lightning
+COPY --chown=user:group --from=rust-Lightning /usr/local/cargo/bin/lightning-hashpool /usr/local/bin/lightning-hashpool
--- src/lightning/payments/bolt11.rs
+++ src/lightning/payments/bolt11.rs
@@ -1,1 +1,43 @@
+use crate::util::error::Error;
+use std::str::FromStr;
+use std::fmt;
 
+use lightning_invoice::Invoice;
+use lightning_invoice::InvoiceError;
+use lightning_invoice::InvoiceFeatures;
+use lightning_invoice::BOLT11;
+use lightning_invoice::InvoiceSignature;
+use lightning_invoice::InvoiceSignatureHashType;
+
+pub struct LightningInvoice {
+    pub invoice: Invoice,
+    pub signature: InvoiceSignature,
+}
+
+impl LightningInvoice {
+    pub fn new(invoice: Invoice, signature: InvoiceSignature) -> Self {
+        LightningInvoice {
+            invoice,
+            signature,
+        }
+    }
+
+pub fn get_payment_hash(&self) -> Result<[u8; 32], Error> {
+        let payment_hash = self.invoice.payment_hash()?;
+        Ok(payment_hash)
+    }
+
+    pub fn get_payment_secret(&self) -> Result<[u8; 32], Error> {
+        let payment_secret = self.invoice.payment_secret()?;
+        Ok(payment_secret)
+    }
+
+    pub fn get_description(&self) -> Result<String, Error> {
+        let description = self.invoice.description();
+        Ok(description)
+    }
+
+    pub fn get_description_hash(&self) -> Result<[u8; 32], Error> {
+        let description_hash = self.invoice.description_hash()?;
+        Ok(description_hash)
+    }
