deaafae870205a8ee3df6870c7443a7339a7aee3
223880 <rafael.segala45@gmail.com>
2024-06-24 07:37:09 -0300

    Initial Node server

--- src/lightning/server/node.rs
+++ src/lightning/server/node.rs
@@ -1,1 +1,35 @@
+use crate::server::Server;
+
+pub struct Node {
+    pub server: Server,
+}
+
+impl Node {
+    pub fn new() -> Node {
+        Node {
+            server: Server::new(),
+        }
+    }
+}
+
+impl Node {
+    pub fn start(&self) {
+        self.server.start();
+    }
+}
+
+impl Drop for Node {
+    fn drop(&mut self) {
+        self.server.stop();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
 
