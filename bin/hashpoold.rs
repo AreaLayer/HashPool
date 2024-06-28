e0610a75ecd8cd39386338ad9fb0b1356859e369
223880 <rafael.segala45@gmail.com>
2024-06-28 07:48:08 -0300

    Fix bin

---
+++ bin/hashpoold.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+
+pub type Cache = Arc<Mutex<HashMap<String, String>>>;
+pub type CacheGuard<'a> = MutexGuard<'a, HashMap<String, String>>;
+
+pub fn new_cache() -> Cache {
+    Arc::new(Mutex::new(HashMap::new()))
+}
+
+pub fn get_cache_guard(cache: &Cache) -> CacheGuard {
+    cache.lock().unwrap()
+}
+
+pub fn get_cache_value(cache: &Cache, key: &str) -> Option<String> {
+    let guard = get_cache_guard(cache);
+    guard.get(key).map(|v| v.clone())
+}
+
+pub fn set_cache_value(cache: &Cache, key: &str, value: &str) {
+    let mut guard = get_cache_guard(cache);
+
+    guard.insert(key.to_string(), value.to_string());
