1722ef9043b90a0a048c5056780181037d6684ee
223880 <rafael.segala45@gmail.com>
2024-08-12 07:00:00 -0300

    Fix

--- src/mining/lib.rs
+++ src/mining/lib.rs
@@ -1,5 +1,5 @@
-use crypto::digest::Digest;
-use crypto::Sha256::Sha256;
+extern use crypto::digest::Digest;
+extern use crypto::Sha256::Sha256;
 
 use std::collections::HashMap;
 use std::sync::Arc;
