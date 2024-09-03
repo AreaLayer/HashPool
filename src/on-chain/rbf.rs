67ce8b67029bd0a237f4eef4e6dcabfd0e612f50
223880 <rafael.segala45@gmail.com>
2024-09-03 08:59:58 -0300

    Fix

--- src/on-chain/rbf.rs
+++ src/on-chain/rbf.rs
@@ -5,16 +5,16 @@ use std::io::BufReader;
 
 use bitcoin::BumpFee;
 use bitcoin::RBF;
-use bitcoin::consensus::encode::deserialize;
-use bitcoin::consensus::decode::deserialize;
+use bitcoin::consensus::encode::Deserialize;
+use bitcoin::consensus::decode::Deserialize;
 use bitcoin::network::constants::Network;
 use bitcoin::Transaction;
 
-fn BumpFee {
+fn main() {
     let mut file = File::open("transaction.bin").unwrap();
     let mut buf = Vec::new();
     file.read_to_end(&mut buf).unwrap();
-    let tx:::Transaction = deserialize(&buf).unwrap();
+    let tx::Transaction = deserialize(&buf).unwrap();
     let mut tx = tx;
     tx.rbf = true;
     tx.bump_fee(1000).unwrap();
@@ -24,12 +24,11 @@ fn BumpFee {
     println!("Transaction hash: {}", tx.txid());
 
 }
-
-impl BumpFee = {
+impl BumpFee {
     fn bump_fee(&mut self, fee_rate: u64) -> Result<Transaction, Error> {
         let mut rbf = RBF::new(self);
         rbf.set_fee_rate(fee_rate);
-        rbf.bump_fee()
+        rbf.bump_fee()?;
         Ok(rbf.transaction)
-}
+    }
 }
