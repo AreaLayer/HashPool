d42d6bc25d75c9fb3d314623b4e68c82d5577742
223880 <rafael.segala45@gmail.com>
2024-09-10 10:44:58 -0300

    Fix lib

--- src/data/lib.rs
+++ src/data/lib.rs
@@ -1,13 +1,12 @@
 use crate::types::types::*;
 use crate::types::traits::*;
-use crate::types::Data:;
+use crate::types::Data;
 
-fn main Data {
+fn main() {
     let x = 5;
     let y = 6;
     let z = x + y;
     println!("Hello, world! {} + {} = {}", x, y, z);
-
 }
 
 impl Data {
