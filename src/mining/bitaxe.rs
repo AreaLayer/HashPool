66e76499a3f9340b0d60a54c54189409ae119c1b
22388o <22388o@users.noreply.github.com>
2025-01-14 08:16:31 -0300

    Add transactions to Bitaxe

--- src/mining/bitaxe.rs
+++ src/mining/bitaxe.rs
@@ -1,6 +1,31 @@
 use crate::block::Block;
 use crate::transaction::Transaction;
+
 pub struct BitAxe {
     pub blocks: Vec<Block>,
     pub transactions: Vec<Transaction>,
+}
+
+pub fn new() -> BitAxe {
+    BitAxe {
+        blocks: Vec::new(),
+        transactions: Vec::new(),
+    }
+}
+
+impl BitAxe {
+    pub fn add_block(&mut self, block: Block) {
+        self.blocks.push(block);
+    }
+
+pub fn add_transaction(&mut self, transaction: Transaction) {
+        self.transactions.push(transaction);
+    }
+
+pub fn get_blocks(&self) -> &Vec<Block> {
+        &self.blocks
+    }
+pub fn get_transactions(&self) -> &Vec<Transaction> {
+        &self.transactions
+    }
 }
